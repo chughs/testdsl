330e65adf40eda7ef975a31359f6d04de1f7fba6
chughs <replytoswati@gmail.com>
2016-04-24 22:23:18 -0700

    Test jobs using dsl

---
+++ testdsl.groovy
@@ -0,0 +1,42 @@
+import groovy.json.JsonSlurper
+def slurper = new JsonSlurper()
+
+def jobsJson = new JsonSlurper().parseText( new URL( 'https://api.github.com/repos/chughs/spark/git/trees/master?' ).text )
+//println jobsJson
+def giturl = "http://github.com/mthak/spark.git"
+def branch = "master"
+def command = "-e clean deploy -DskipTests"
+jobsJson.tree.each { 
+if (it.type == 'tree' && it.path != '.github') {
+   path = it.path
+   println "Creating jobs " + path
+mavenJob("APM-${it.path}") {
+    scm {
+        git(giturl,branch)
+    }
+    triggers {
+     scm('*/15 * * * *')
+    }
+        rootPOM("${path}/pom.xml")
+        goals(command)
+    /*steps {
+        maven(command)
+    }*/
+}
+}
+}
+categorizedJobsView('APM-Jobs') {
+    jobs {
+        regex(/APM-.*/)
+    }
+    categorizationCriteria {
+        regexGroupingRule(/^APM-.*$/, namingRule="APM-Master")
+
+    }
+    description("APM-Master")
+    columns {
+        status()
+        categorizedJob()
+        buildButton()
+    }
+}
