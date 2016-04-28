cdbb110ab55c7074afe7751ae9302e996f1958d1
chughs <replytoswati@gmail.com>
2016-04-28 09:52:31 -0700

    Update testdsl.groovy

--- testdsl.groovy
+++ testdsl.groovy
@@ -8,7 +8,7 @@ def branch = "master"
 def command = "-e clean deploy -DskipTests"
 def jobsJson = slurper.parseText(readFileFromWorkspace("test.json"))
 
-jobsJson.projects.each { team,APM -> 
+jobsJson.APM.each { team,APM -> 
      jobnames=APM
      folder(team) {
          displayName(team)
@@ -29,35 +29,3 @@ jobsJson.projects.each { team,APM ->
    }
 }
 }
-/*if (it.type == 'tree' && it.path != '.github') {
-   path = it.path
-   println "Creating jobs " + path
-mavenJob("APM-${it.path}") {
-    scm {
-        git(giturl,branch)
-    }
-    triggers {
-    }
-        rootPOM("${path}/pom.xml")
-        goals(command)
-    /*steps {
-        maven(command)
-    }
-}
-}
-}*/
-///*categorizedJobsView('APM-Jobs') {
-  //  jobs {
-    //    regex(/APM-.*/)*/
-    //}/*
-    //categorizationCriteria {
-      //  regexGroupingRule(/^APM-.*$/, namingRule="APM-Master")
-
-    //}
-    //description("APM-Master")
-    //columns {
-     //   status()
-      //  categorizedJob()
-       // buildButton()
-    //}
-//}*/
